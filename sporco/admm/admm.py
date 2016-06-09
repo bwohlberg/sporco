Fixed bug in primal residual calculation.

sporco/admm/admm.py
@@ -315,14 +315,16 @@ class ADMM(object):
     def relax_AX(self):
         """Implement relaxation if option ``RelaxParam`` != 1.0."""
 
+        # We need to keep the non-relaxed version of AX since it is
+        # required for computation of primal residual r
+        self.AXnr = self.cnst_A(self.X)
         if self.opt['RelaxParam'] == 1.0:
-            self.AX = self.cnst_A(self.X)
+            self.AX = self.AXnr
         else:
             if not hasattr(self, 'c'):
                 self.c = self.cnst_c()
             alpha = self.opt['RelaxParam']
-            self.AX = alpha*self.cnst_A(self.X) - \
-                (1-alpha)*(self.cnst_B(self.Y) - self.c)
+            self.AX = alpha*self.AXnr - (1-alpha)*(self.cnst_B(self.Y) - self.c)
 
 
 
@@ -331,16 +333,16 @@ class ADMM(object):
         """Compute residuals and stopping thresholds."""
 
         if self.opt['AutoRho', 'StdResiduals']:
-            r = linalg.norm(self.rsdl_r(self.AX, self.Y))
+            r = linalg.norm(self.rsdl_r(self.AXnr, self.Y))
             s = linalg.norm(self.rsdl_s(self.Yprev, self.Y))
             epri = scipy.sqrt(self.Nc)*self.opt['AbsStopTol'] + \
-                self.rsdl_rn(self.AX, self.Y)*self.opt['RelStopTol']
+                self.rsdl_rn(self.AXnr, self.Y)*self.opt['RelStopTol']
             edua = scipy.sqrt(self.Nx)*self.opt['AbsStopTol'] + \
                 self.rsdl_sn(self.U)*self.opt['RelStopTol']
         else:
-            rn = self.rsdl_rn(self.AX, self.Y)
+            rn = self.rsdl_rn(self.AXnr, self.Y)
             sn = self.rsdl_sn(self.U)
-            r = linalg.norm(self.rsdl_r(self.AX, self.Y)) / rn
+            r = linalg.norm(self.rsdl_r(self.AXnr, self.Y)) / rn
             s = linalg.norm(self.rsdl_s(self.Yprev, self.Y)) / sn
             epri = scipy.sqrt(self.Nc)*self.opt['AbsStopTol']/rn + \
                 self.opt['RelStopTol']
@@ -389,8 +391,6 @@ class ADMM(object):
                 self.U = self.U/rsf
                 if rsf != 1.0:
                     self.rhochange()
-
-
 
 
 
@@ -586,7 +586,6 @@ class ADMM(object):
 
 
 
-
 class ADMMEqual(ADMM):
     """Base class for ADMM algorithms with a simple equality constraint.
 
@@ -666,6 +665,7 @@ class ADMMEqual(ADMM):
     def relax_AX(self):
         """Implement relaxation if option ``RelaxParam`` != 1.0."""
 
+        self.AXnr = self.X
         if self.opt['RelaxParam'] == 1.0:
             self.AX = self.X
         else:

sporco/admm/tests/test_bpdn.py
@@ -58,13 +58,13 @@ class TestSet01(object):
         s0 = D.dot(x0)
         lmbda = 5e-3
         opt = bpdn.BPDN.Options({'Verbose' : False, 'MaxMainIter' : 500,
-                    'RelStopTol' : 1e-3})
+                                 'RelStopTol' : 5e-4})
         b = bpdn.BPDN(D, s0, lmbda, opt)
         b.solve()
         x1 = b.Y
-        assert(np.abs(b.itstat[-1].ObjFun - 1.2016e-2) < 1e-5)
-        assert(np.abs(b.itstat[-1].DFid - 1.0025e-5) < 1e-5)
-        assert(np.abs(b.itstat[-1].RegL1 - 2.40116) < 1e-5)
+        assert(np.abs(b.itstat[-1].ObjFun - 0.012009) < 1e-5)
+        assert(np.abs(b.itstat[-1].DFid - 1.9636082e-06) < 1e-5)
+        assert(np.abs(b.itstat[-1].RegL1 - 2.401446) < 1e-5)
         assert(linalg.norm(x1-x0) < 1e-3)
 
 

sporco/admm/tests/test_rpca.py
@@ -28,8 +28,8 @@ class TestSet01(object):
                               'AutoRho' : {'Enabled' : True}})
         b = rpca.RobustPCA(D, None, opt)
         X, Y = b.solve()
-        assert(np.abs(b.itstat[-1].ObjFun - 321.6189484339) < 1e-6)
-        assert(sl.mse(U,X) < 2e-6)
+        assert(np.abs(b.itstat[-1].ObjFun - 321.493968419) < 1e-6)
+        assert(sl.mse(U,X) < 5e-6)
         assert(sl.mse(V,Y) < 1e-8)
 
 

sporco/admm/tests/test_spline.py
@@ -25,12 +25,12 @@ class TestSet01(object):
         D = U + V
         lmbda = 0.1
         opt = spline.SplineL1.Options({'Verbose' : False, 'gEvalY' : False,
-                              'MaxMainIter' : 250,
+                              'MaxMainIter' : 250, 'RelStopTol' : 5e-4,
                               'DFidWeight' : V == 0, 
                               'AutoRho' : {'Enabled' : True}})
         b = spline.SplineL1(D, lmbda, opt)
         X = b.solve()
-        assert(np.abs(b.itstat[-1].ObjFun - 0.38397223518) < 1e-6)
+        assert(np.abs(b.itstat[-1].ObjFun - 0.333606246) < 1e-6)
         assert(sl.mse(U,X) < 1e-6)
 
 

sporco/admm/tests/test_tvl2.py
@@ -66,7 +66,7 @@ class TestSet02(object):
                                        'MaxMainIter' : 250})
         b = tvl2.TVL2Deconv(np.ones((1)), self.D, lmbda, opt)
         X = b.solve()
-        assert(np.abs(b.itstat[-1].ObjFun - 45.441537456677) < 1e-3)
+        assert(np.abs(b.itstat[-1].ObjFun - 45.45958573088) < 1e-3)
         assert(sl.mse(self.U,X) < 1e-3)
 
 
@@ -89,7 +89,7 @@ class TestSet03(object):
                                         'MaxMainIter' : 250, 'rho' : 10*lmbda})
         b = tvl2.TVL2Denoise(self.D, lmbda, opt, axes=(0,1))
         X = b.solve()
-        assert(np.abs(b.itstat[-1].ObjFun - 363.03797264834986) < 1e-3)
+        assert(np.abs(b.itstat[-1].ObjFun - 363.0802047) < 1e-3)
         assert(sl.mse(self.U,X) < 1e-3)
 
 
@@ -99,7 +99,7 @@ class TestSet03(object):
                                        'MaxMainIter' : 250})
         b = tvl2.TVL2Deconv(np.ones((1)), self.D, lmbda, opt, axes=(0,1))
         X = b.solve()
-        assert(np.abs(b.itstat[-1].ObjFun - 563.96797296313059) < 1e-3)
+        assert(np.abs(b.itstat[-1].ObjFun - 564.1586542) < 1e-3)
         assert(sl.mse(self.U,X) < 1e-3)
 
 
@@ -132,5 +132,5 @@ class TestSet04(object):
                                        'MaxMainIter' : 250})
         b = tvl2.TVL2Deconv(np.ones((1)), self.D, lmbda, opt, axes=(0,1,2))
         X = b.solve()
-        assert(np.abs(b.itstat[-1].ObjFun - 567.49092052210574) < 1e-3)
+        assert(np.abs(b.itstat[-1].ObjFun - 567.72425227) < 1e-3)
         assert(sl.mse(self.U,X) < 1e-3)