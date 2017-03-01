276bf5e6eb406c78278c9677fc73607f94af787d
Brendt Wohlberg <brendt.wohlberg@gmail.com>
2017-02-28 20:29:03 -0700

    Minor plot improvements, added mpldatacursor use to imview function

--- sporco/plot.py
+++ sporco/plot.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-# Copyright (C) 2015-2016 by Brendt Wohlberg <brendt@ieee.org>
+# Copyright (C) 2015-2017 by Brendt Wohlberg <brendt@ieee.org>
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SPORCO package. Details of the copyright
 # and user license can be found in the 'LICENSE.txt' file distributed
@@ -40,34 +40,34 @@ def plot(dat, x=None, title=None, xlbl=None, ylbl=None, lgnd=None, lglc=None,
     ----------
     dat : array_like
         Data to plot. Each column is plotted as a separate curve.
-    x : array_like, optional (default=None)
+    x : array_like, optional (default None)
         Values for x-axis of the plot
-    title : string, optional (default=None)
+    title : string, optional (default None)
         Figure title
-    xlbl : string, optional (default=None)
+    xlbl : string, optional (default None)
         Label for x-axis
-    ylbl : string, optional (default=None)
+    ylbl : string, optional (default None)
         Label for y-axis
-    lgnd : list of strings, optional (default=None)
+    lgnd : list of strings, optional (default None)
         List of legend string
-    lglc : string, optional (default=None)
+    lglc : string, optional (default None)
         Legend location string
-    ptyp : string, optional (default='plot')
+    ptyp : string, optional (default 'plot')
         Plot type specification (options are 'plot', 'semilogx', 'semilogy',
         and 'loglog')
-    lwidth : float, optional (default=1.0)
+    lwidth : float, optional (default 1.0)
         Line width
-    lstyle : string, optional (default='solid')
+    lstyle : string, optional (default 'solid')
         Line style (see :class:`matplotlib.lines.Line2D`)
-    block : boolean, optional (default=False)
+    block : boolean, optional (default False)
         If True, the function only returns when the figure is closed
-    fgrf : figure object reference, optional (default=None)
+    fgrf : figure object reference, optional (default None)
         Draw in specified figure instead of creating one
-    fgnm : integer, optional (default=None)
+    fgnm : integer, optional (default None)
         Figure number of figure
-    fgsz : tuple (width,height), optional (default=(12,12))
+    fgsz : tuple (width,height), optional (default (12,12))
         Specify figure dimensions in inches.
-    cbar : boolean, optional (default=False)
+    cbar : boolean, optional (default False)
         Flag indicating whether to display colorbar
 
     Returns
@@ -126,29 +126,29 @@ def surf(z, x=None, y=None, title=None, xlbl=None, ylbl=None, zlbl=None,
     ----------
     z : array_like
         2d array of data to plot
-    x : array_like, optional (default=None)
+    x : array_like, optional (default None)
         Values for x-axis of the plot
-    y : array_like, optional (default=None)
+    y : array_like, optional (default None)
         Values for y-axis of the plot
-    title : string, optional (default=None)
+    title : string, optional (default None)
         Figure title
-    xlbl : string, optional (default=None)
+    xlbl : string, optional (default None)
         Label for x-axis
-    ylbl : string, optional (default=None)
+    ylbl : string, optional (default None)
         Label for y-axis
-    zlbl : string, optional (default=None)
+    zlbl : string, optional (default None)
         Label for z-axis
-    block : boolean, optional (default=False)
+    block : boolean, optional (default False)
         If True, the function only returns when the figure is closed
-    cmap : matplotlib.cm colormap, optional (default=None)
+    cmap : matplotlib.cm colormap, optional (default None)
         Colour map for surface. If none specifed, defaults to cm.coolwarm
-    fgrf : figure object reference, optional (default=None)
+    fgrf : figure object reference, optional (default None)
         Draw in specified figure instead of creating one
-    axrf : axes object reference, optional (default=None)
+    axrf : axes object reference, optional (default None)
         Plot in specified axes instead of creating one
-    fgnm : integer, optional (default=None)
+    fgnm : integer, optional (default None)
         Figure number of figure
-    fgsz : tuple (width,height), optional (default=None)
+    fgsz : tuple (width,height), optional (default None)
         Specify figure dimensions in inches.
 
     Returns
@@ -203,8 +203,9 @@ def surf(z, x=None, y=None, title=None, xlbl=None, ylbl=None, zlbl=None,
 
 
 
-def imview(img, title=None, block=False, cmap=None, fgrf=None, fgnm=None,
-           fgsz=(12, 12), intrp='nearest', cbar=False, axes=None):
+def imview(img, title=None, block=False, copy=True, fltscl=False, cmap=None,
+           fgrf=None, fgnm=None, fgsz=(12, 12), intrp='nearest', cbar=False,
+           axes=None):
     """
     Display an image.
 
@@ -217,22 +218,29 @@ def imview(img, title=None, block=False, cmap=None, fgrf=None, fgnm=None,
     ----------
     img : array_like, shape (Nr, Nc) or (Nr, Nc, 3) or (Nr, Nc, 4)
         Image to display.
-    title : string, optional (default=None)
+    title : string, optional (default None)
         Figure title
-    block : boolean, optional (default=False)
+    block : boolean, optional (default False)
         If True, the function only returns when the figure is closed
-    cmap : matplotlib.cm colormap, optional (default=None)
+    copy : boolean, optional (default True)
+        If True, create a copy of input `img` as a reference for displayed
+        pixel values, ensuring that displayed values do not change when the
+        array changes in the calling scope. Set this flag to False if the
+        overhead of an additional copy of the input image is not acceptable.
+    fltscl : boolean, optional (default False)
+        If True, rescale and shift floating point arrays to [0,1].
+    cmap : matplotlib.cm colormap, optional (default None)
         Colour map for image. If none specifed, defaults to cm.Greys_r
         for monochrome image
-    fgrf : figure object reference, optional (default=None)
+    fgrf : figure object reference, optional (default None)
         Draw in specified figure instead of creating one
-    fgnm : integer, optional (default=None)
+    fgnm : integer, optional (default None)
         Figure number of figure
-    fgsz : tuple (width,height), optional (default=(12,12))
+    fgsz : tuple (width,height), optional (default (12,12))
         Specify figure dimensions in inches.
-    cbar : boolean, optional (default=False)
+    cbar : boolean, optional (default False)
         Flag indicating whether to display colorbar
-    axes : matplotlib.axes.Axes object, optional (default=None)
+    axes : matplotlib.axes.Axes object, optional (default None)
         If specified the new figure shares axes with the specified axes of
         an existing figure so that a zoom is shared across both figures
 
@@ -249,13 +257,22 @@ def imview(img, title=None, block=False, cmap=None, fgrf=None, fgnm=None,
         raise ValueError('Argument img must be an Nr x Nc array or an '
                          'Nr x Nc x 3 array')
 
-    imgd = np.copy(img)
+    imgd = img.copy()
+    if copy:
+        # Keep a separate copy of the input image so that the original
+        # pixel values can be display rather than the scaled pixel
+        # values that are actually plotted.
+        img = img.copy()
 
     if cmap is None and img.ndim == 2:
         cmap = cm.Greys_r
 
-    if np.issubdtype(img.dtype, np.float) and img.ndim > 2:
-        imgd = np.clip(imgd, 0.0, 1.0)
+    if np.issubdtype(img.dtype, np.float):
+        if fltscl:
+            imgd -= imgd.min()
+            imgd /= imgd.max()
+        if img.ndim > 2:
+            imgd = np.clip(imgd, 0.0, 1.0)
     elif img.dtype == np.uint16:
         imgd = np.float16(imgd) / np.iinfo(np.uint16).max
     elif img.dtype == np.int16:
@@ -287,14 +304,14 @@ def imview(img, title=None, block=False, cmap=None, fgrf=None, fgnm=None,
         col = int(x+0.5)
         row = int(y+0.5)
         if col >= 0 and col < nc and row >= 0 and row < nr:
-            z = imgd[row, col]
+            z = img[row, col]
             if imgd.ndim == 2:
                 return 'x=%.2f, y=%.2f, z=%.2f' % (x, y, z)
             else:
                 return 'x=%.2f, y=%.2f, z=(%.2f,%.2f,%.2f)' % \
                     sum(((x,), (y,), tuple(z)), ())
         else:
-            return 'x=%.2f, y=%.2f'%(x, y)
+            return 'x=%.2f, y=%.2f' % (x, y)
 
     def press(event):
         if event.key == 'q':
@@ -305,6 +322,9 @@ def imview(img, title=None, block=False, cmap=None, fgrf=None, fgnm=None,
     plt.axis('off')
     ax = plt.gca()
     ax.format_coord = format_coord
+
+    if have_mpldc:
+        mpldc.datacursor(display='single')
 
     if fgrf is None:
         plt.show(block=block)
@@ -319,7 +339,7 @@ def close(fgrf=None):
 
     Parameters
     ----------
-    fgrf : figure object reference or integer or None, optional (default=None)
+    fgrf : figure object reference or integer or None, optional (default None)
         If a figure object reference or figure number is provided, close the
         specified figure, otherwise close all figures.
     """
