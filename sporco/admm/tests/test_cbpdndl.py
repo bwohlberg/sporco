bcbf6d83e14139ed8b7b7b80422c55a585f0e2f1
Brendt Wohlberg <brendt.wohlberg@gmail.com>
2017-05-05 07:39:11 -0600

    Faster tests and new test for multichannel case

--- sporco/admm/tests/test_cbpdndl.py
+++ sporco/admm/tests/test_cbpdndl.py
@@ -22,9 +22,10 @@ class TestSet01(object):
 
     def test_01(self):
         lmbda = 1e-1
+        opt = cbpdndl.ConvBPDNDictLearn.Options({'MaxMainIter' : 10})
         try:
             b = cbpdndl.ConvBPDNDictLearn(self.D0, self.S[...,0], lmbda,
-                                          dimK=0)
+                                          opt=opt, dimK=0)
             b.solve()
         except Exception as e:
             print(e)
@@ -33,8 +34,9 @@ class TestSet01(object):
 
     def test_02(self):
         lmbda = 1e-1
+        opt = cbpdndl.ConvBPDNDictLearn.Options({'MaxMainIter' : 10})
         try:
-            b = cbpdndl.ConvBPDNDictLearn(self.D0, self.S, lmbda)
+            b = cbpdndl.ConvBPDNDictLearn(self.D0, self.S, lmbda, opt=opt)
             b.solve()
         except Exception as e:
             print(e)
@@ -42,8 +44,9 @@ class TestSet01(object):
 
 
     def test_03(self):
+        opt = cbpdndl.ConvBPDNDictLearn.Options({'MaxMainIter' : 10})
         try:
-            b = cbpdndl.ConvBPDNDictLearn(self.D0, self.S)
+            b = cbpdndl.ConvBPDNDictLearn(self.D0, self.S, opt=opt)
             b.solve()
         except Exception as e:
             print(e)
@@ -51,6 +54,24 @@ class TestSet01(object):
 
 
     def test_04(self):
+        N = 16
+        Nc = 3
+        Nd = 5
+        M = 4
+        K = 3
+        D0 = np.random.randn(Nd, Nd, Nc, M)
+        S = np.random.randn(N, N, Nc, K)
+        lmbda = 1e-1
+        opt = cbpdndl.ConvBPDNDictLearn.Options({'MaxMainIter': 10})
+        try:
+            b = cbpdndl.ConvBPDNDictLearn(D0, S, lmbda, opt=opt)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_05(self):
         lmbda = 1e-1
         opt = cbpdndl.ConvBPDNDictLearn.Options({'AccurateDFid' : True,
                                                  'MaxMainIter' : 10})
