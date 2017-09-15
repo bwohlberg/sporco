ee61d3a24ee38c7756284e7baad30d6f0b87c814
Brendt Wohlberg <brendt@ieee.org>
2017-09-14 18:16:52 -0600

    Tests and examples for new modules/classes

---
+++ sporco/admm/tests/test_ccmodmd.py
@@ -0,0 +1,309 @@
+from builtins import object
+
+import pytest
+import numpy as np
+
+from sporco.admm import ccmodmd
+import sporco.linalg as sl
+
+
+
+class TestSet01(object):
+
+    def setup_method(self, method):
+        np.random.seed(12345)
+
+
+    def test_01(self):
+        N = 16
+        M = 4
+        Nd = 8
+        X = np.random.randn(N, N, 1, 1, M)
+        S = np.random.randn(N, N)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_IterSM.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_IterSM(X, S, W, (Nd, Nd, 1, M),
+                                           opt=opt, dimK=0)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_02(self):
+        N = 16
+        K = 3
+        M = 4
+        Nd = 8
+        X = np.random.randn(N, N, 1, K, M)
+        S = np.random.randn(N, N, K)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_IterSM.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_IterSM(X, S, W,
+                                    (Nd, Nd, 1, M), opt=opt)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_03(self):
+        N = 16
+        M = 4
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, 1, M)
+        S = np.random.randn(N, N, Nc)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_IterSM.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_IterSM(X, S, W, (Nd, Nd, 1, M),
+                                           opt=opt, dimK=0)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_04(self):
+        N = 16
+        M = 4
+        K = 2
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, K, M)
+        S = np.random.randn(N, N, Nc, K)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_IterSM.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_IterSM(X, S, W, (Nd, Nd, 1, M),
+                                           opt=opt)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_05(self):
+        N = 16
+        M = 4
+        K = 2
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, K, M)
+        S = np.random.randn(N, N, Nc, K)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_IterSM.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_IterSM(X, S, W, (Nd, Nd, Nc, M),
+                                           opt=opt)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_06(self):
+        N = 16
+        M = 4
+        Nd = 8
+        X = np.random.randn(N, N, 1, 1, M)
+        S = np.random.randn(N, N)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_CG.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_CG(X, S, W, (Nd, Nd, 1, M),
+                                           opt=opt, dimK=0)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_07(self):
+        N = 16
+        K = 3
+        M = 4
+        Nd = 8
+        X = np.random.randn(N, N, 1, K, M)
+        S = np.random.randn(N, N, K)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_CG.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_CG(X, S, W,
+                                    (Nd, Nd, 1, M), opt=opt)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_08(self):
+        N = 16
+        M = 4
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, 1, M)
+        S = np.random.randn(N, N, Nc)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_CG.Options(
+                           {'Verbose' : False, 'MaxMainIter' : 20,
+                            'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_CG(X, S, W, (Nd, Nd, 1, M),
+                                           opt=opt, dimK=0)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_09(self):
+        N = 16
+        M = 4
+        Nd = 8
+        X = np.random.randn(N, N, 1, 1, M)
+        S = np.random.randn(N, N)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_Consensus.Options(
+                {'Verbose' : False, 'MaxMainIter' : 20,
+                 'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_Consensus(X, S, W,
+                                (Nd, Nd, 1, M), opt=opt, dimK=0)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_10(self):
+        N = 16
+        K = 3
+        M = 4
+        Nd = 8
+        X = np.random.randn(N, N, 1, K, M)
+        S = np.random.randn(N, N, K)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_Consensus.Options(
+                {'Verbose' : False, 'MaxMainIter' : 20,
+                 'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_Consensus(X, S, W,
+                                        (Nd, Nd, 1, M), opt=opt)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_11(self):
+        N = 16
+        M = 4
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, 1, M)
+        S = np.random.randn(N, N, Nc)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_Consensus.Options(
+                {'Verbose' : False, 'MaxMainIter' : 20,
+                 'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_Consensus(X, S, W,
+                                (Nd, Nd, 1, M), opt=opt, dimK=0)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_12(self):
+        N = 16
+        M = 4
+        K = 2
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, K, M)
+        S = np.random.randn(N, N, Nc, K)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_Consensus.Options(
+                {'Verbose' : False, 'MaxMainIter' : 20,
+                 'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_Consensus(X, S, W,
+                                (Nd, Nd, 1, M), opt=opt)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_13(self):
+        N = 16
+        M = 4
+        K = 2
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, K, M)
+        S = np.random.randn(N, N, Nc, K)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcpl_Consensus.Options(
+                {'Verbose' : False,  'MaxMainIter' : 20,
+                 'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl_Consensus(X, S, W,
+                                (Nd, Nd, Nc, M), opt=opt)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_14(self):
+        opt = ccmodmd.ConvCnstrMODMaskDcplBase.Options({'AuxVarObj' : False})
+        assert(opt['fEvalX'] is True and opt['gEvalY'] is False)
+        opt['AuxVarObj'] = True
+        assert(opt['fEvalX'] is False and opt['gEvalY'] is True)
+
+
+    def test_15(self):
+        opt = ccmodmd.ConvCnstrMODMaskDcplBase.Options({'AuxVarObj' : True})
+        assert(opt['fEvalX'] is False and opt['gEvalY'] is True)
+        opt['AuxVarObj'] = False
+        assert(opt['fEvalX'] is True and opt['gEvalY'] is False)
+
+
+    def test_16(self):
+        N = 16
+        M = 4
+        Nc = 3
+        Nd = 8
+        X = np.random.randn(N, N, Nc, 1, M)
+        S = np.random.randn(N, N, Nc)
+        W = 1.0
+        try:
+            opt = ccmodmd.ConvCnstrMODMaskDcplOptions({'Verbose' : False,
+                            'MaxMainIter' : 20, 'LinSolveCheck' : True})
+            c = ccmodmd.ConvCnstrMODMaskDcpl(X, S, W, (Nd, Nd, 1, M),
+                                             opt=opt, dimK=0)
+            c.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+        assert(np.array(c.getitstat().XSlvRelRes).max() < 1e-5)
---
+++ sporco/admm/tests/test_parcnsdl.py
@@ -0,0 +1,63 @@
+from __future__ import division
+from builtins import object
+
+import pytest
+import numpy as np
+
+from sporco.admm import parcnsdl
+
+
+
+class TestSet01(object):
+
+    def setup_method(self, method):
+        np.random.seed(12345)
+        N = 16
+        Nd = 5
+        M = 4
+        K = 3
+        self.D0 = np.random.randn(Nd, Nd, M)
+        self.S = np.random.randn(N, N, K)
+
+
+    def test_01(self):
+        lmbda = 1e-1
+        opt = parcnsdl.ConvBPDNDictLearn_Consensus.Options({'MaxMainIter': 10})
+        try:
+            b = parcnsdl.ConvBPDNDictLearn_Consensus(self.D0, self.S[...,0],
+                                            lmbda, opt=opt, nproc=2, dimK=0)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_02(self):
+        lmbda = 1e-1
+        opt = parcnsdl.ConvBPDNDictLearn_Consensus.Options({'MaxMainIter': 10})
+        try:
+            b = parcnsdl.ConvBPDNDictLearn_Consensus(self.D0, self.S, lmbda,
+                                                     opt=opt, nproc=2)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
+    def test_03(self):
+        N = 16
+        Nc = 3
+        Nd = 5
+        M = 4
+        K = 3
+        D0 = np.random.randn(Nd, Nd, Nc, M)
+        S = np.random.randn(N, N, Nc, K)
+        lmbda = 1e-1
+        opt = parcnsdl.ConvBPDNDictLearn_Consensus.Options({'MaxMainIter': 10})
+        try:
+            b = parcnsdl.ConvBPDNDictLearn_Consensus(D0, S, lmbda, opt=opt,
+                                                     nproc=2)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert(0)
