e4a716b32b675d6e23ba0bfc3b2d7c6f9bc5d7a3
Brendt Wohlberg <brendt@ieee.org>
2018-01-19 12:03:01 -0700

    Fix numpy version issues in einsum interface

--- sporco/linalg.py
+++ sporco/linalg.py
@@ -313,10 +313,14 @@ def inner(x, y, axis=-1):
         xr = np.rollaxis(x, axis, 0)
         yr = np.rollaxis(y, axis, 0)
 
-    # Efficient inner product on axis 0 (setting of optimize
-    # flag due to https://github.com/numpy/numpy/issues/10343)
-    ip = np.einsum(xr, [0, Ellipsis], yr, [0, Ellipsis],
-                   optimize=False)[np.newaxis, ...]
+    # Efficient inner product on axis 0
+    if np.__version__ == '1.14.0':
+        # Setting of optimize flag due to
+        #    https://github.com/numpy/numpy/issues/10343
+        ip = np.einsum(xr, [0, Ellipsis], yr, [0, Ellipsis],
+                       optimize=False)[np.newaxis, ...]
+    else:
+        ip = np.einsum(xr, [0, Ellipsis], yr, [0, Ellipsis])[np.newaxis, ...]
 
     # Roll axis back to original position if necessary
     if axis != 0:
