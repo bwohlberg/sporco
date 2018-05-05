44e3519eba40b430c3def3ea11bb13dc59f7aa97
Brendt Wohlberg <brendt@ieee.org>
2018-05-05 08:46:40 -0600

    Trivial edit

--- sporco/util.py
+++ sporco/util.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015-2017 by Brendt Wohlberg <brendt@ieee.org>
+# Copyright (C) 2015-2018 by Brendt Wohlberg <brendt@ieee.org>
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SPORCO package. Details of the copyright
 # and user license can be found in the 'LICENSE.txt' file distributed
