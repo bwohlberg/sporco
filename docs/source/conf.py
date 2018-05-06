c1c112a35e70b6d2f00a0c0a5cffb5aa053af159
Brendt Wohlberg <brendt@ieee.org>
2018-05-06 07:30:12 -0600

    Updated docs copyright dates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SPORCO'
-copyright = u'2015-2017, Brendt Wohlberg'
+copyright = u'2015-2018, Brendt Wohlberg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
