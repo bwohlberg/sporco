d88ea85c21b96c981b59a1b00fb72ad9b8150c1d
Brendt Wohlberg <brendt@ieee.org>
2018-08-10 18:01:12 -0600

    Added tests for onlinecdl.OnlineConvBPDNMaskDictLearn

--- sporco/dictlrn/tests/test_onlinecdl.py
+++ sporco/dictlrn/tests/test_onlinecdl.py
@@ -46,6 +46,20 @@ class TestSet01(object):
             assert(0)
 
 
+    def test_03(self):
+        lmbda = 1e-1
+        W = np.random.randn(*self.S.shape[0:2])
+        opt = onlinecdl.OnlineConvBPDNMaskDictLearn.Options()
+        try:
+            b = onlinecdl.OnlineConvBPDNMaskDictLearn(self.D0, lmbda, opt=opt)
+            for it in range(10):
+                img_index = np.random.randint(0, self.S.shape[-1])
+                b.solve(self.S[..., img_index], W)
+        except Exception as e:
+            print(e)
+            assert(0)
+
+
 
 class TestSet02(object):
 
@@ -86,3 +100,16 @@ class TestSet02(object):
             print(e)
             assert(0)
 
+
+    def test_03(self):
+        lmbda = 1e-1
+        W = np.random.randn(*self.S.shape[0:3])
+        opt = onlinecdl.OnlineConvBPDNMaskDictLearn.Options()
+        try:
+            b = onlinecdl.OnlineConvBPDNMaskDictLearn(self.D0, lmbda, opt=opt)
+            for it in range(10):
+                img_index = np.random.randint(0, self.S.shape[-1])
+                b.solve(self.S[..., img_index], W)
+        except Exception as e:
+            print(e)
+            assert(0)
