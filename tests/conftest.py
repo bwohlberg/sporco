d7b0c31344a0fa5c360569f2369aabb9e6ad837a
Brendt Wohlberg <brendt@ieee.org>
2018-09-27 20:49:37 -0600

    Improved test skip configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,18 @@
 import matplotlib as mpl
 mpl.use('Agg')
 
+
+collect_ignore = []
 try:
     import importlib.util
 except ImportError:
-    collect_ignore = ['cupy']
+    collect_ignore.append('cupy')
+try:
+    import cupy
+except ImportError:
+    if 'cupy' not in collect_ignore:
+        collect_ignore.append('cupy')
+try:
+    import mpi4py
+except ImportError:
+    collect_ignore.append('test_mpiutil.py')
