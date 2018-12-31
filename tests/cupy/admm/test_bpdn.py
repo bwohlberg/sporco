b91d06ae459e0d7f4d2abaf303e5cb948ba34cd3
Brendt Wohlberg <brendt@ieee.org>
2018-12-31 09:55:45 -0700

    Additional tests

---
+++ tests/cupy/admm/test_bpdn.py
@@ -0,0 +1,375 @@
+from __future__ import division
+from builtins import object
+
+import pickle
+import pytest
+import numpy as np
+try:
+    import cupy as cp
+    try:
+        cp.cuda.Device(0).compute_capability
+    except cp.cuda.runtime.CUDARuntimeError:
+        pytest.skip("GPU device inaccessible", allow_module_level=True)
+except ImportError:
+    pytest.skip("cupy not installed", allow_module_level=True)
+
+
+from sporco.cupy.admm import bpdn
+from sporco.cupy.util import list2array
+
+
+
+def CallbackTest(obj):
+    return bool(obj.k > 5)
+
+
+
+class TestSet01(object):
+
+    def setup_method(self, method):
+        cp.random.seed(12345)
+
+
+    def test_01(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        lmbda = 1e-1
+        try:
+            b = bpdn.BPDN(D, s, lmbda)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+
+
+    def test_02(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        try:
+            b = bpdn.BPDN(D, s)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+
+
+    def test_03(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        try:
+            opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 20,
+                                     'LinSolveCheck': True,
+                                     'AutoRho': {'StdResiduals': True}})
+            b = bpdn.BPDN(D, s, lmbda=1.0, opt=opt)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+        assert list2array(b.getitstat().XSlvRelRes).max() < 1e-5
+
+
+    def test_04(self):
+        N = 8
+        M = 8
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        try:
+            opt = bpdn.BPDN.Options(
+                {'FastSolve': True, 'Verbose': False, 'MaxMainIter': 10,
+                 'AutoRho': {'Enabled': False}})
+            b = bpdn.BPDN(D, s, lmbda=1.0, opt=opt)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+
+
+    def test_05(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        dt = cp.float16
+        opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 20,
+                                 'AutoRho': {'Enabled': True},
+                                 'DataType': dt})
+        b = bpdn.BPDN(D, s, lmbda=1.0, opt=opt)
+        b.solve()
+        assert b.X.dtype == dt
+        assert b.Y.dtype == dt
+        assert b.U.dtype == dt
+
+
+    def test_06(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        dt = cp.float32
+        opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 20,
+                                 'AutoRho': {'Enabled': True},
+                                 'DataType': dt})
+        b = bpdn.BPDN(D, s, lmbda=1.0, opt=opt)
+        b.solve()
+        assert b.X.dtype == dt
+        assert b.Y.dtype == dt
+        assert b.U.dtype == dt
+
+
+    def test_07(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        dt = cp.float64
+        opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 20,
+                                 'AutoRho': {'Enabled': True},
+                                 'DataType': dt})
+        b = bpdn.BPDN(D, s, lmbda=1.0, opt=opt)
+        b.solve()
+        assert b.X.dtype == dt
+        assert b.Y.dtype == dt
+        assert b.U.dtype == dt
+
+
+    def test_08(self):
+        N = 64
+        M = 2 * N
+        L = 4
+        np.random.seed(12345)
+        D = cp.array(np.random.randn(N, M))
+        x0 = cp.zeros((M, 1))
+        si = cp.array(np.random.permutation(M))
+        x0[si[0:L]] = cp.array(np.random.randn(L, 1))
+        s0 = D.dot(x0)
+        lmbda = 5e-3
+        opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 500,
+                                 'RelStopTol': 5e-4})
+        b = bpdn.BPDN(D, s0, lmbda, opt)
+        b.solve()
+        x1 = b.Y
+        assert cp.abs(b.itstat[-1].ObjFun - 0.012009) < 1e-5
+        assert cp.abs(b.itstat[-1].DFid - 1.9636082e-06) < 1e-5
+        assert cp.abs(b.itstat[-1].RegL1 - 2.401446) < 1e-5
+        assert cp.linalg.norm(x1 - x0) < 1e-3
+
+
+    def test_09(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        lmbda = 1e-1
+        mu = 1e-2
+        try:
+            b = bpdn.BPDNJoint(D, s, lmbda, mu)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+
+
+    def test_10(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        dt = cp.float16
+        opt = bpdn.BPDNJoint.Options({'Verbose': False, 'MaxMainIter': 20,
+                                      'AutoRho': {'Enabled': True},
+                                      'DataType': dt})
+        b = bpdn.BPDNJoint(D, s, lmbda=1.0, mu=0.1, opt=opt)
+        b.solve()
+        assert b.X.dtype == dt
+        assert b.Y.dtype == dt
+        assert b.U.dtype == dt
+
+
+    def test_11(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        lmbda = 1e-1
+        mu = 1e-2
+        try:
+            b = bpdn.ElasticNet(D, s, lmbda, mu)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+
+
+    def test_12(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        lmbda = 1e-1
+        mu = 1e-2
+        try:
+            opt = bpdn.ElasticNet.Options(
+                {'Verbose': False, 'MaxMainIter': 20, 'LinSolveCheck': True,
+                 'AutoRho': {'StdResiduals': True}})
+            b = bpdn.ElasticNet(D, s, lmbda, mu, opt=opt)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+        assert list2array(b.getitstat().XSlvRelRes).max() < 1e-5
+
+
+    def test_13(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        dt = cp.float16
+        opt = bpdn.ElasticNet.Options({'Verbose': False, 'MaxMainIter': 20,
+                                       'AutoRho': {'Enabled': True},
+                                       'DataType': dt})
+        b = bpdn.ElasticNet(D, s, lmbda=1.0, mu=0.1, opt=opt)
+        b.solve()
+        assert b.X.dtype == dt
+        assert b.Y.dtype == dt
+        assert b.U.dtype == dt
+
+
+#    def test_14(self):
+#        N = 8
+#        M = 16
+#        D = cp.random.randn(N, M)
+#        s = cp.random.randn(N, 1)
+#        gamma = 1e0
+#        try:
+#            b = bpdn.BPDNProjL1(D, s, gamma)
+#            b.solve()
+#        except Exception as e:
+#            print(e)
+#            assert 0
+
+
+#    def test_15(self):
+#        N = 8
+#        M = 16
+#        D = cp.random.randn(N, M)
+#        s = cp.random.randn(N, 1)
+#        dt = cp.float16
+#        opt = bpdn.BPDNProjL1.Options({'Verbose': False, 'MaxMainIter': 20,
+#                                       'AutoRho': {'Enabled': True},
+#                                       'DataType': dt})
+#        b = bpdn.BPDNProjL1(D, s, gamma=1.0, opt=opt)
+#        b.solve()
+#        assert b.X.dtype == dt
+#        assert b.Y.dtype == dt
+#        assert b.U.dtype == dt
+
+
+    def test_16(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        epsilon = 1e-1
+        try:
+            b = bpdn.MinL1InL2Ball(D, s, epsilon)
+            b.solve()
+        except Exception as e:
+            print(e)
+            assert 0
+
+
+    def test_17(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        dt = cp.float16
+        opt = bpdn.MinL1InL2Ball.Options({'Verbose': False, 'MaxMainIter': 20,
+                                          'AutoRho': {'Enabled': True},
+                                          'DataType': dt})
+        b = bpdn.MinL1InL2Ball(D, s, epsilon=1.0, opt=opt)
+        b.solve()
+        assert b.X.dtype == dt
+        assert b.Y.dtype == dt
+        assert b.U.dtype == dt
+
+
+    def test_18(self):
+        N = 64
+        M = 2 * N
+        L = 8
+        cp.random.seed(12345)
+        D = cp.random.randn(N, M)
+        x0 = cp.zeros((M, 1))
+        si = cp.random.permutation(M)
+        x0[si[0:L]] = cp.random.randn(L, 1)
+        s = D.dot(x0)
+        lmbda = 5e-2
+        opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 300,
+                                 'RelStopTol': 1e-5, 'AutoRho':
+                                 {'Enabled': False}})
+        bp = bpdn.BPDN(D, s, lmbda=lmbda, opt=opt)
+        Xp = bp.solve()
+        epsilon = cp.linalg.norm(D.dot(Xp) - s)
+        opt = bpdn.MinL1InL2Ball.Options(
+            {'Verbose': False, 'MaxMainIter': 300, 'RelStopTol': 1e-5,
+             'rho': 2e1, 'AutoRho': {'Enabled': False}})
+        bc = bpdn.MinL1InL2Ball(D, s, epsilon=epsilon, opt=opt)
+        Xc = bc.solve()
+        assert cp.linalg.norm(Xp - Xc) / cp.linalg.norm(Xp) < 1e-3
+        assert cp.abs(cp.linalg.norm(Xp, 1) - cp.linalg.norm(Xc, 1)) < 1e-3
+
+
+    def test_19(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        lmbda = 1e-1
+        opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 10})
+        b = bpdn.BPDN(D, s, lmbda, opt)
+        bp = pickle.dumps(b)
+        c = pickle.loads(bp)
+        Xb = b.solve()
+        Xc = c.solve()
+        assert cp.linalg.norm(Xb - Xc) == 0.0
+
+
+    def test_20(self):
+        opt = bpdn.GenericBPDN.Options({'AuxVarObj': False})
+        assert opt['fEvalX'] is True and opt['gEvalY'] is False
+        opt['AuxVarObj'] = True
+        assert opt['fEvalX'] is False and opt['gEvalY'] is True
+
+
+    def test_21(self):
+        opt = bpdn.GenericBPDN.Options({'AuxVarObj': True})
+        assert opt['fEvalX'] is False and opt['gEvalY'] is True
+        opt['AuxVarObj'] = False
+        assert opt['fEvalX'] is True and opt['gEvalY'] is False
+
+
+    @pytest.mark.filterwarnings('ignore:admm.ADMM.runtime')
+    def test_22(self):
+        N = 8
+        M = 16
+        D = cp.random.randn(N, M)
+        s = cp.random.randn(N, 1)
+        lmbda = 1e-1
+        opt = bpdn.BPDN.Options({'Verbose': False, 'MaxMainIter': 10,
+                                 'Callback': CallbackTest, 'RelaxParam': 1.0})
+        b = bpdn.BPDN(D, s, lmbda, opt=opt)
+        assert b.getitstat() is None
+        b.solve()
+        assert b.runtime > 0.0
+        assert b.k == 7
+        assert b.var_x() is not None
+        assert b.var_y() is not None
+        assert b.var_u() is not None
---
+++ tests/cupy/test_linalg.py
@@ -0,0 +1,194 @@
+from __future__ import division
+from builtins import object
+
+import pytest
+try:
+    import cupy as cp
+    try:
+        cp.cuda.Device(0).compute_capability
+    except cp.cuda.runtime.CUDARuntimeError:
+        pytest.skip("GPU device inaccessible", allow_module_level=True)
+except ImportError:
+    pytest.skip("cupy not installed", allow_module_level=True)
+
+
+import cupy as cp
+from sporco.cupy import linalg
+from sporco.cupy import util
+
+
+
+class TestSet01(object):
+
+    def setup_method(self, method):
+        cp.random.seed(12345)
+
+
+    def test_05(self):
+        rho = 1e-1
+        N = 64
+        M = 128
+        K = 32
+        D = cp.random.randn(N, M)
+        X = cp.random.randn(M, K)
+        S = D.dot(X)
+        Z = (D.T.dot(D).dot(X) + rho*X - D.T.dot(S)) / rho
+        c, lwr = linalg.cho_factor(D, rho)
+        Xslv = linalg.cho_solve_ATAI(D, rho, D.T.dot(S) + rho*Z, c, lwr)
+        assert(linalg.rrs(D.T.dot(D).dot(Xslv) + rho*Xslv,
+                        D.T.dot(S) + rho*Z) < 1e-11)
+
+
+
+    def test_06(self):
+        rho = 1e-1
+        N = 128
+        M = 64
+        K = 32
+        D = cp.random.randn(N, M)
+        X = cp.random.randn(M, K)
+        S = D.dot(X)
+        Z = (D.T.dot(D).dot(X) + rho*X - D.T.dot(S)) / rho
+        c, lwr = linalg.cho_factor(D, rho)
+        Xslv = linalg.cho_solve_ATAI(D, rho, D.T.dot(S) + rho*Z, c, lwr)
+        assert(linalg.rrs(D.T.dot(D).dot(Xslv) + rho*Xslv,
+                        D.T.dot(S) + rho*Z) < 1e-14)
+
+
+
+    def test_07(self):
+        rho = 1e-1
+        N = 64
+        M = 128
+        K = 32
+        D = cp.random.randn(N, M)
+        X = cp.random.randn(M, K)
+        S = D.dot(X)
+        Z = (D.dot(X).dot(X.T) + rho*D - S.dot(X.T)) / rho
+        c, lwr = linalg.cho_factor(X, rho)
+        Dslv = linalg.cho_solve_AATI(X, rho, S.dot(X.T) + rho*Z, c, lwr)
+        assert(linalg.rrs(Dslv.dot(X).dot(X.T) + rho*Dslv,
+                        S.dot(X.T) + rho*Z) < 1e-11)
+
+
+
+    def test_08(self):
+        rho = 1e-1
+        N = 128
+        M = 64
+        K = 32
+        D = cp.random.randn(N, M)
+        X = cp.random.randn(M, K)
+        S = D.dot(X)
+        Z = (D.dot(X).dot(X.T) + rho*D - S.dot(X.T)) / rho
+        c, lwr = linalg.cho_factor(X, rho)
+        Dslv = linalg.cho_solve_AATI(X, rho, S.dot(X.T) + rho*Z, c, lwr)
+        assert(linalg.rrs(Dslv.dot(X).dot(X.T) + rho*Dslv,
+                        S.dot(X.T) + rho*Z) < 1e-11)
+
+
+
+    def test_09(self):
+        rho = 1e-1
+        N = 32
+        M = 16
+        K = 8
+        D = util.complex_randn(N, N, 1, 1, M)
+        X = util.complex_randn(N, N, 1, K, M)
+        S = cp.sum(D*X, axis=4, keepdims=True)
+        Z = (D.conj()*cp.sum(D*X, axis=4, keepdims=True) + \
+             rho*X - D.conj()*S) / rho
+        Xslv = linalg.solvedbi_sm(D, rho, D.conj()*S + rho*Z)
+        assert(linalg.rrs(D.conj()*cp.sum(D*Xslv, axis=4, keepdims=True) +
+                        rho*Xslv, D.conj()*S + rho*Z) < 1e-11)
+
+
+
+    def test_10(self):
+        N = 32
+        M = 16
+        K = 8
+        D = util.complex_randn(N, N, 1, 1, M)
+        X = util.complex_randn(N, N, 1, K, M)
+        S = cp.sum(D*X, axis=4, keepdims=True)
+        d = 1e-1 * (cp.random.randn(N, N, 1, 1, M).astype('complex') +
+            cp.random.randn(N, N, 1, 1, M).astype('complex') * 1.0j)
+        Z = (D.conj()*cp.sum(D*X, axis=4, keepdims=True) +
+             d*X - D.conj()*S) / d
+        Xslv = linalg.solvedbd_sm(D, d, D.conj()*S + d*Z)
+        assert(linalg.rrs(D.conj()*cp.sum(D*Xslv, axis=4, keepdims=True) +
+                        d*Xslv, D.conj()*S + d*Z) < 1e-11)
+
+
+
+    def test_11(self):
+        rho = 1e-1
+        N = 32
+        M = 16
+        K = 8
+        D = util.complex_randn(N, N, 1, 1, M)
+        X = util.complex_randn(N, N, 1, K, M)
+        S = cp.sum(D*X, axis=4, keepdims=True)
+
+        Xop = lambda x: cp.sum(X * x, axis=4, keepdims=True)
+        XHop = lambda x: cp.sum(cp.conj(X) * x, axis=3, keepdims=True)
+        Z = (XHop(Xop(D)) + rho*D - XHop(S)) / rho
+        Dslv = linalg.solvemdbi_ism(X, rho, XHop(S) + rho*Z, 4, 3)
+
+        assert linalg.rrs(XHop(Xop(Dslv)) + rho*Dslv, XHop(S) + rho*Z) < 1e-11
+
+
+
+    def test_12(self):
+        rho = 1e-1
+        N = 32
+        M = 16
+        C = 3
+        K = 8
+        D = util.complex_randn(N, N, C, 1, M)
+        X = util.complex_randn(N, N, 1, K, M)
+        S = cp.sum(D*X, axis=4, keepdims=True)
+
+        Xop = lambda x: cp.sum(X * x, axis=4, keepdims=True)
+        XHop = lambda x: cp.sum(cp.conj(X)* x, axis=3, keepdims=True)
+        Z = (XHop(Xop(D)) + rho*D - XHop(S)) / rho
+        Dslv = linalg.solvemdbi_ism(X, rho, XHop(S) + rho*Z, 4, 3)
+
+        assert linalg.rrs(XHop(Xop(Dslv)) + rho*Dslv, XHop(S) + rho*Z) < 1e-11
+
+
+
+    def test_13(self):
+        rho = 1e-1
+        N = 32
+        M = 16
+        K = 8
+        D = util.complex_randn(N, N, 1, 1, M)
+        X = util.complex_randn(N, N, 1, K, M)
+        S = cp.sum(D*X, axis=4, keepdims=True)
+
+        Xop = lambda x: cp.sum(X * x, axis=4, keepdims=True)
+        XHop = lambda x: cp.sum(cp.conj(X) * x, axis=3, keepdims=True)
+        Z = (XHop(Xop(D)) + rho*D - XHop(S)) / rho
+        Dslv = linalg.solvemdbi_rsm(X, rho, XHop(S) + rho*Z, 3)
+
+        assert linalg.rrs(XHop(Xop(Dslv)) + rho*Dslv, XHop(S) + rho*Z) < 1e-11
+
+
+
+    def test_14(self):
+        rho = 1e-1
+        N = 64
+        M = 32
+        C = 3
+        K = 8
+        D = util.complex_randn(N, N, C, 1, M)
+        X = util.complex_randn(N, N, 1, K, M)
+        S = cp.sum(D*X, axis=4, keepdims=True)
+
+        Xop = lambda x: cp.sum(X * x, axis=4, keepdims=True)
+        XHop = lambda x: cp.sum(cp.conj(X) * x, axis=3, keepdims=True)
+        Z = (XHop(Xop(D)) + rho*D - XHop(S)) / rho
+        Dslv = linalg.solvemdbi_rsm(X, rho, XHop(S) + rho*Z, 3)
+
+        assert linalg.rrs(XHop(Xop(Dslv)) + rho*Dslv, XHop(S) + rho*Z) < 1e-11
