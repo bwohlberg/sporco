0f80921939ac5fa3461e9c518be038a1899d3e8f
Brendt Wohlberg <brendt@ieee.org>
2020-10-09 17:31:37 -0600

    Cleanup

--- sporco/cupy/pgm/__init__.py
+++ sporco/cupy/pgm/__init__.py
@@ -5,7 +5,7 @@
 # and user license can be found in the 'LICENSE.txt' file distributed
 # with the package.
 
-"""Construct variant of pgm subpackage that use cupy instead of numpy"""
+"""Construct variant of pgm subpackage that use cupy instead of numpy."""
 
 from __future__ import absolute_import
 
@@ -32,17 +32,17 @@ pgm.momentum = sporco_cupy_patch_module('sporco.pgm.momentum')
 pgm.stepsize = sporco_cupy_patch_module('sporco.pgm.stepsize')
 
 # Construct sporco.cupy.pgm.pgm
-pgm.pgm = sporco_cupy_patch_module('sporco.pgm.pgm',
-             {'IterativeSolver': common.IterativeSolver,
-              'rfftn': fft.rfftn, 'irfftn': fft.irfftn,
-              'BacktrackStandard': pgm.backtrack.BacktrackStandard,
-              'BacktrackRobust': pgm.backtrack.BacktrackRobust,
-              'MomentumNesterov': pgm.momentum.MomentumNesterov,
-              'MomentumLinear': pgm.momentum.MomentumLinear,
-              'MomentumGenLinear': pgm.momentum.MomentumGenLinear,
-              'StepSizePolicyCauchy': pgm.stepsize.StepSizePolicyCauchy,
-              'StepSizePolicyBB': pgm.stepsize.StepSizePolicyBB
-              })
+pgm.pgm = sporco_cupy_patch_module(
+    'sporco.pgm.pgm',
+    {'IterativeSolver': common.IterativeSolver,
+     'rfftn': fft.rfftn, 'irfftn': fft.irfftn,
+     'BacktrackStandard': pgm.backtrack.BacktrackStandard,
+     'BacktrackRobust': pgm.backtrack.BacktrackRobust,
+     'MomentumNesterov': pgm.momentum.MomentumNesterov,
+     'MomentumLinear': pgm.momentum.MomentumLinear,
+     'MomentumGenLinear': pgm.momentum.MomentumGenLinear,
+     'StepSizePolicyCauchy': pgm.stepsize.StepSizePolicyCauchy,
+     'StepSizePolicyBB': pgm.stepsize.StepSizePolicyBB})
 
 
 # Record current entries in sys.modules and then replace them with
@@ -57,13 +57,14 @@ sys.modules['sporco.pgm.pgm'] = pgm.pgm
 
 
 # Construct sporco.cupy.pgm.cbpdn
-pgm.cbpdn = sporco_cupy_patch_module('sporco.pgm.cbpdn',
-            {'pgm': pgm.pgm, 'inner':  linalg.inner,
-             'CSC_ConvRepIndexing': cnvrep.CSC_ConvRepIndexing,
-             'mskWshape': cnvrep.mskWshape, 'rfftn': fft.rfftn,
-             'irfftn': fft.irfftn, 'empty_aligned': fft.empty_aligned,
-             'rfftn_empty_aligned': fft.rfftn_empty_aligned,
-             'rfl2norm2': fft.rfl2norm2, 'prox_l1': prox.prox_l1})
+pgm.cbpdn = sporco_cupy_patch_module(
+    'sporco.pgm.cbpdn',
+    {'pgm': pgm.pgm, 'inner': linalg.inner,
+     'CSC_ConvRepIndexing': cnvrep.CSC_ConvRepIndexing,
+     'mskWshape': cnvrep.mskWshape, 'rfftn': fft.rfftn,
+     'irfftn': fft.irfftn, 'empty_aligned': fft.empty_aligned,
+     'rfftn_empty_aligned': fft.rfftn_empty_aligned,
+     'rfl2norm2': fft.rfl2norm2, 'prox_l1': prox.prox_l1})
 
 
 # Restore original entries in sys.modules
