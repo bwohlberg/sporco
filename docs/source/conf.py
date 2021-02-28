85f23e111e633a40c173fead3c92257ff06d3bdc
Brendt Wohlberg <brendt@ieee.org>
2021-02-28 07:28:45 -0700

    Bump copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,7 +110,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SPORCO'
-copyright = u'2015-2020, Brendt Wohlberg'
+copyright = u'2015-2021, Brendt Wohlberg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
