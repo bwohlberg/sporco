61f3a9e1c230e6b9c4d4148a180ab99e29fc634c
Brendt Wohlberg <brendt@ieee.org>
2025-10-08 08:53:04 -0600

    Clean up

--- sporco/__init__.py
+++ sporco/__init__.py
@@ -1,8 +1,3 @@
 from __future__ import absolute_import
 
 __version__ = '0.2.2a2'
-
-
-# This is a temporary solution to the circular imports resulting from the
-# use of the renamed_function decorator
-import sporco.linalg
